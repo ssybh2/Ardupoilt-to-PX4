486a559ea16a5162f2c8536f611e87d9c2fe4887
Boyang Hu <ssybh2@nottingham.edu.cn>
2026-09-14 17:27:49 +0800

    refactor: use uploaded DSun geometric controller interface

--- l1_adaptive_control/GeometricController.hpp
+++ l1_adaptive_control/GeometricController.hpp
@@ -7,101 +7,65 @@
 class GeometricController
 {
 public:
-struct Input {
-hrt_abstime timestamp_us{0};
+	// Defaults are the real-aircraft values from the uploaded DSun controller and
+	// the REAL_OR_SITL branch of L1Quad.  The PX4 orchestrator may explicitly
+	// load the SITL source values when REAL_OR_SITL == 0.
+	struct Parameters {
+		float mass_kg{0.62f};
+		float position_gain[3]{14.0f, 15.0f, 15.0f};
+		float velocity_gain[3]{1.5f, 0.9f, 1.1f};
+		float rotation_gain[3]{0.55f, 0.35f, 0.15f};
+		float angular_velocity_gain[3]{0.035f, 0.03f, 0.004f};
+		float inertia_kg_m2[3]{0.002016f, 0.001827f, 0.00322f};
+	};
 
-float position_ned[3]{0.f, 0.f, 0.f};
-float velocity_ned[3]{0.f, 0.f, 0.f};
-float quat_body_to_ned[4]{1.f, 0.f, 0.f, 0.f};
-float angular_velocity_body[3]{0.f, 0.f, 0.f};
+	struct Input {
+		hrt_abstime timestamp_us{0};
 
-float target_position_ned[3]{0.f, 0.f, 0.f};
-float target_velocity_ned[3]{0.f, 0.f, 0.f};
-float target_acceleration_ned[3]{0.f, 0.f, 0.f};
-float target_jerk_ned[3]{0.f, 0.f, 0.f};
-float target_snap_ned[3]{0.f, 0.f, 0.f};
+		float position_ned[3]{0.f, 0.f, 0.f};
+		float velocity_ned[3]{0.f, 0.f, 0.f};
+		float quat_body_to_ned[4]{1.f, 0.f, 0.f, 0.f};
+		float angular_velocity_body[3]{0.f, 0.f, 0.f};
 
-float target_yaw{0.f};
-float target_yaw_rate{0.f};
-float target_yaw_accel{0.f};
+		float target_position_ned[3]{0.f, 0.f, 0.f};
+		float target_velocity_ned[3]{0.f, 0.f, 0.f};
+		float target_acceleration_ned[3]{0.f, 0.f, 0.f};
+		float target_jerk_ned[3]{0.f, 0.f, 0.f};
+		float target_snap_ned[3]{0.f, 0.f, 0.f};
 
-bool state_valid_for_control{false};
-bool armed{false};
-bool failsafe{false};
-uint8_t nav_state{0};
-};
+		// Keep the original ModeAdaptive API: yaw is represented by
+		// [cos(yaw), sin(yaw)] and its first/second derivatives.
+		float target_yaw[2]{1.f, 0.f};
+		float target_yaw_dot[2]{0.f, 0.f};
+		float target_yaw_ddot[2]{0.f, 0.f};
 
-struct Output {
-hrt_abstime timestamp_us{0};
+		bool state_valid_for_control{false};
+		bool armed{false};
+		bool failsafe{false};
+		uint8_t nav_state{0};
+	};
 
-float position_error_ned[3]{0.f, 0.f, 0.f};
-float velocity_error_ned[3]{0.f, 0.f, 0.f};
+	struct Output {
+		hrt_abstime timestamp_us{0};
+		float r_error[3]{0.f, 0.f, 0.f};
+		float v_error[3]{0.f, 0.f, 0.f};
+		float target_thrust{0.f};
+		float M[3]{0.f, 0.f, 0.f};
+		bool valid{false};
+	};
 
-float acceleration_error_ned[3]{0.f, 0.f, 0.f};
-float jerk_error_ned[3]{0.f, 0.f, 0.f};
+	GeometricController() = default;
+	~GeometricController() = default;
 
-float target_force_ned[3]{0.f, 0.f, 0.f};
-float target_force_dot_ned[3]{0.f, 0.f, 0.f};
-float target_force_ddot_ned[3]{0.f, 0.f, 0.f};
+	bool update(const Input &input, Output &output);
+	void set_parameters(const Parameters &parameters) { _parameters = parameters; }
 
-float body_z_axis_ned[3]{0.f, 0.f, 1.f};
-float body_z_axis_dot_ned[3]{0.f, 0.f, 0.f};
-float target_thrust_dot_newton_s{0.f};
-
-float desired_body_x_axis_ned[3]{1.f, 0.f, 0.f};
-float desired_body_y_axis_ned[3]{0.f, 1.f, 0.f};
-float desired_body_z_axis_ned[3]{0.f, 0.f, 1.f};
-float desired_body_x_axis_dot_ned[3]{0.f, 0.f, 0.f};
-float desired_body_y_axis_dot_ned[3]{0.f, 0.f, 0.f};
-float desired_body_z_axis_dot_ned[3]{0.f, 0.f, 0.f};
-float desired_body_x_axis_ddot_ned[3]{0.f, 0.f, 0.f};
-float desired_body_y_axis_ddot_ned[3]{0.f, 0.f, 0.f};
-float desired_body_z_axis_ddot_ned[3]{0.f, 0.f, 0.f};
-
-float desired_rotation_matrix[3][3]{{1.f, 0.f, 0.f}, {0.f, 1.f, 0.f}, {0.f, 0.f, 1.f}};
-float desired_rotation_matrix_dot[3][3]{{0.f, 0.f, 0.f}, {0.f, 0.f, 0.f}, {0.f, 0.f, 0.f}};
-float desired_rotation_matrix_ddot[3][3]{{0.f, 0.f, 0.f}, {0.f, 0.f, 0.f}, {0.f, 0.f, 0.f}};
-
-// Stage: unit_vec(-target_force, -target_force_dot, -target_force_ddot)
-float b3c_ned[3]{0.f, 0.f, 1.f};
-float b3c_dot_ned[3]{0.f, 0.f, 0.f};
-float b3c_ddot_ned[3]{0.f, 0.f, 0.f};
-
-// Stage: A2 and unit_vec(A2, A2_dot, A2_ddot)
-float a2_ned[3]{0.f, 1.f, 0.f};
-float a2_dot_ned[3]{0.f, 0.f, 0.f};
-float a2_ddot_ned[3]{0.f, 0.f, 0.f};
-
-float b2c_ned[3]{0.f, 1.f, 0.f};
-float b2c_dot_ned[3]{0.f, 0.f, 0.f};
-float b2c_ddot_ned[3]{0.f, 0.f, 0.f};
-
-float rotation_error[3]{0.f, 0.f, 0.f};
-
-float desired_angular_velocity_body[3]{0.f, 0.f, 0.f};
-float desired_angular_acceleration_body[3]{0.f, 0.f, 0.f};
-float angular_velocity_error[3]{0.f, 0.f, 0.f};
-
-float pd_moment_newton_meter[3]{0.f, 0.f, 0.f};
-float feedforward_moment_newton_meter[3]{0.f, 0.f, 0.f};
-float j_omega_body[3]{0.f, 0.f, 0.f};
-float gyro_moment_newton_meter[3]{0.f, 0.f, 0.f};
-
-float thrust_newton{0.f};
-float moment_newton_meter[3]{0.f, 0.f, 0.f};
-
-bool valid{false};
-};
-
-GeometricController() = default;
-~GeometricController() = default;
-
-bool update(const Input &input, Output &output);
-
-const Input &last_input() const { return _last_input; }
-const Output &last_output() const { return _last_output; }
+	const Input &last_input() const { return _last_input; }
+	const Output &last_output() const { return _last_output; }
+	const Parameters &parameters() const { return _parameters; }
 
 private:
-Input _last_input{};
-Output _last_output{};
+	Parameters _parameters{};
+	Input _last_input{};
+	Output _last_output{};
 };
