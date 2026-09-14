96e06f103fd84bae15e34842680901ef052ad646
Boyang Hu <ssybh2@nottingham.edu.cn>
2026-09-14 17:29:56 +0800

    test: use PX4 matrix vector access in mixer parity tests

--- l1_adaptive_control/MotorMixerTest.cpp
+++ l1_adaptive_control/MotorMixerTest.cpp
@@ -17,13 +17,13 @@ TEST(MotorMixer, SitlHoverMatchesOriginalThreeIterationMixer)
 	mixer.set_parameters(parameters);
 
 	MotorMixer::ThrustMoment command{};
-	command[0] = kHoverThrust;
+	command(0) = kHoverThrust;
 
 	MotorMixer::MotorCommand output{};
 	ASSERT_TRUE(mixer.mix(command, output));
 
 	for (int i = 0; i < 4; i++) {
-		EXPECT_NEAR(output[i], 57.57798f, 2e-4f);
+		EXPECT_NEAR(output(i), 57.57798f, 2e-4f);
 	}
 }
 
@@ -35,14 +35,14 @@ TEST(MotorMixer, SitlRollMomentKeepsOriginalXLayoutSigns)
 	mixer.set_parameters(parameters);
 
 	MotorMixer::ThrustMoment command{};
-	command[0] = kHoverThrust;
-	command[1] = 0.1f;
+	command(0) = kHoverThrust;
+	command(1) = 0.1f;
 
 	MotorMixer::MotorCommand output{};
 	ASSERT_TRUE(mixer.mix(command, output));
 
-	EXPECT_NEAR(output[0], 56.62741f, 3e-4f);
-	EXPECT_NEAR(output[1], 58.51626f, 3e-4f);
-	EXPECT_NEAR(output[2], 58.51626f, 3e-4f);
-	EXPECT_NEAR(output[3], 56.62741f, 3e-4f);
+	EXPECT_NEAR(output(0), 56.62741f, 3e-4f);
+	EXPECT_NEAR(output(1), 58.51626f, 3e-4f);
+	EXPECT_NEAR(output(2), 58.51626f, 3e-4f);
+	EXPECT_NEAR(output(3), 56.62741f, 3e-4f);
 }
